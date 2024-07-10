acfaacb3a74185b56547e413e2737ef385d21eea
David Renshaw <dwrenshaw@gmail.com>
2024-07-10 09:03:38 -0400

    [Imo1987P4] style tweaks

--- Input/Imo1987P4.lean
+++ Input/Imo1987P4.lean
@@ -32,7 +32,7 @@ theorem imo1987_p4 : ¬∃ f : ℕ → ℕ, ∀ n, f (f n) = n + 1987 := by
 
   -- Note that f is injective, because if f(x) = f(y),
   -- then f(f(x)) = f(f(y)), so x = y.
-  have f_injective : f.Injective := by
+  have f_inj : f.Injective := by
     intro x y hxy;
     have hfx := hf x;
     rw [hxy, hf y] at hfx
@@ -43,12 +43,12 @@ theorem imo1987_p4 : ¬∃ f : ℕ → ℕ, ∀ n, f (f n) = n + 1987 := by
   let B : Set ℕ := f '' A
 
   -- A ∪ B = {0, 1, ... , 2 * m}.
-  have ab_range : A ∪ B = {n | n < 2*m + 1} := by
-    refine calc A ∪ B
-              = Set.univ \ (f '' (f '' Set.univ)) := ?_
-            _ = {n | n < 2*m + 1} := ?_
+  have ab_range : A ∪ B = {n | n < 2 * m + 1} := by
+    calc A ∪ B
+       = Set.univ \ (f '' (f '' Set.univ)) := ?_
+       _ = {n | n < 2 * m + 1} := ?_
     · -- Note that B = f(ℕ) - f(f(ℕ)).
-      simp only [B, Set.image_diff f_injective]
+      simp only [B, Set.image_diff f_inj]
       apply Set.eq_of_subset_of_subset
       · rintro x (hx1 | hx2) <;> aesop
       · rintro x ⟨_hx, hx'⟩
@@ -66,7 +66,7 @@ theorem imo1987_p4 : ¬∃ f : ℕ → ℕ, ∀ n, f (f n) = n + 1987 := by
         aesop
 
   -- A and B are disjoint.
-  have ab_disjoint : Disjoint A B := by
+  have ab_disj : Disjoint A B := by
     intro C hca hcb c hc
     exact Set.not_mem_of_mem_diff (hca hc) (Set.image_subset f sdiff_le (hcb hc))
 
@@ -82,7 +82,7 @@ theorem imo1987_p4 : ¬∃ f : ℕ → ℕ, ∀ n, f (f n) = n + 1987 := by
     have ab_finite : (A ∪ B).Finite := by
       rw [ab_range]; exact Set.finite_lt_nat _
     obtain ⟨a_finite, b_finite⟩ := Set.finite_union.mp ab_finite
-    rwa [Set.ncard_union_eq ab_disjoint a_finite b_finite] at ab_card
+    rwa [Set.ncard_union_eq ab_disj a_finite b_finite] at ab_card
 
-  rw [Set.ncard_image_of_injective _ f_injective] at ab_card
+  rw [Set.ncard_image_of_injective _ f_inj] at ab_card
   omega
