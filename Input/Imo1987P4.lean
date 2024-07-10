70de5795832d19980a865c27c05ca0a47c2c2a51
David Renshaw <dwrenshaw@gmail.com>
2024-07-10 08:27:20 -0400

    [Imo1987P4] ab_range calculation

--- Input/Imo1987P4.lean
+++ Input/Imo1987P4.lean
@@ -44,26 +44,26 @@ theorem imo1987_p4 : ¬∃ f : ℕ → ℕ, ∀ n, f (f n) = n + 1987 := by
 
   -- A ∪ B = {0, 1, ... , 2 * m}.
   have ab_range : A ∪ B = {n | n < 2*m + 1} := by
-    -- A ∪ B = ℕ - f(f(ℕ)).
-    have ab_union : A ∪ B = Set.univ \ (f '' (f '' Set.univ)) := by
-      -- Note that B = f(ℕ) - f(f(ℕ)).
+    refine calc A ∪ B
+              = Set.univ \ (f '' (f '' Set.univ)) := ?_
+            _ = {n | n < 2*m + 1} := ?_
+    · -- Note that B = f(ℕ) - f(f(ℕ)).
       simp only [B, Set.image_diff f_injective]
       apply Set.eq_of_subset_of_subset
       · rintro x (hx1 | hx2) <;> aesop
       · rintro x ⟨_hx, hx'⟩
         by_cases (x ∈ A) <;> aesop
-    rw [ab_union]
-    apply Set.eq_of_subset_of_subset
-    · intro x hx
-      replace hx : ∀ (y : ℕ), ¬f (f y) = x := by aesop
-      rw [Set.mem_setOf_eq]
-      by_contra! H
-      obtain ⟨z, hz⟩ : ∃ z, x = (2 * m + 1) + z := exists_add_of_le H
-      have hzz := hx z
-      rw [hz, hf z, add_comm] at hzz
-      exact (hzz rfl).elim
-    · intro x hx
-      aesop
+    · apply Set.eq_of_subset_of_subset
+      · intro x hx
+        replace hx : ∀ (y : ℕ), ¬f (f y) = x := by aesop
+        rw [Set.mem_setOf_eq]
+        by_contra! H
+        obtain ⟨z, hz⟩ : ∃ z, x = (2 * m + 1) + z := exists_add_of_le H
+        have hzz := hx z
+        rw [hz, hf z, add_comm] at hzz
+        exact (hzz rfl).elim
+      · intro x hx
+        aesop
 
   -- A and B are disjoint.
   have ab_disjoint : Disjoint A B := by
