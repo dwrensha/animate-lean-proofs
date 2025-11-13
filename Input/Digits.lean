726d0886f60f36d8d9d6f912cf7b8951998a6a70
David Renshaw <dwrenshaw@gmail.com>
2025-11-13 14:07:17 -0500

    update to lean-v4.23.0

--- Input/Digits.lean
+++ Input/Digits.lean
@@ -11,13 +11,16 @@ theorem digits_len_le_digits_len_succ (b n : ℕ) :
   simpa [digits_len, hb, hn] using log_mono_right (le_succ _)
 
 theorem digit_sum_le (p n : ℕ) : (digits p n).sum ≤ n := by
-  induction' n with n
-  · rw [digits_zero]
+  induction n with
+  | zero =>
+    rw [digits_zero]
     exact Nat.le_refl (List.sum [])
-  · induction' p with p
-    · rw [digits_zero_succ, List.sum_cons]
+  | succ n => induction p with
+    | zero =>
+      rw [digits_zero_succ, List.sum_cons]
       rw [List.sum_nil, add_zero]
-    · nth_rw 2 [← ofDigits_digits p.succ (n + 1)]
+    | succ p =>
+      nth_rw 2 [← ofDigits_digits p.succ (n + 1)]
       rw [← ofDigits_one (digits p.succ n.succ)]
       refine ofDigits_monotone (digits p.succ n.succ) ?_
       exact Nat.succ_pos p
